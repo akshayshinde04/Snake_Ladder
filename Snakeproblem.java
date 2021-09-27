004fc241d3b4d7af13afd77fbaff5ae037f31466
unknown <akshayHVshinde04@gmail.com>
2021-09-27 22:12:42 +0530

    Added: Snake Ladder

---
+++ Snakeproblem.java
@@ -0,0 +1,10 @@
+package snakeladder;
+
+   public class Snakeproblem {
+	   public static void main(String[] args) {
+		   int startposition = 0; 
+	    	 System.out.println("Start position "+startposition);		
+	}
+   }	
+	 		
+
