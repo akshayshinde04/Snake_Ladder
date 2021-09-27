734c3e84933776c345edb62f10758ce99b1392fb
unknown <akshayHVshinde04@gmail.com>
2021-09-27 22:37:30 +0530

    Added:  UC2_RandomNoGenerate

--- Snakeproblem.java
+++ Snakeproblem.java
@@ -2,8 +2,11 @@ package snakeladder;
 
    public class Snakeproblem {
 	   public static void main(String[] args) {
-		   int startposition = 0; 
-	    	 System.out.println("Start position "+startposition);		
+		   int snake=0 , ladder=0;
+	       int startposition = 0; 
+	       System.out.println("Start position "+startposition);	
+	       int dies = (int)((Math.random()*10) % 6)+1;
+	   	   System.out.println("Dies number is:" +dies);
 	}
    }	
 	 		
