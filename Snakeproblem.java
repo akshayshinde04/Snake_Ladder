a4b26105c8ee12926147d457eff3d4afe20244db
unknown <akshayHVshinde04@gmail.com>
2021-09-28 07:04:12 +0530

    Added: UC3_PlayersMove

--- Snakeproblem.java
+++ Snakeproblem.java
@@ -2,12 +2,26 @@ package snakeladder;
 
    public class Snakeproblem {
 	   public static void main(String[] args) {
-		   int snake=0 , ladder=0;
-	       int startposition = 0; 
-	       System.out.println("Start position "+startposition);	
-	       int dies = (int)((Math.random()*10) % 6)+1;
-	   	   System.out.println("Dies number is:" +dies);
+	        int snake=1, ladder=2, player=15;
+	        int dice=(int)(Math.random()*6+1);
+	        System.out.println(dice);
+
+	        int check=(int)(Math.random()*3+1);
+	        if (check==snake) {
+	            player= player-dice;
+	            System.out.println("Snake bite");
+	        }
+	        else if (check==ladder) {
+	            player=player+dice;
+	            System.out.println("U get Ladder");
+	        }
+	        else {
+	            player=player+dice;
+	            System.out.println("NO Play");
+	        }
+	        System.out.println("Player position is: "+player);
+	    }
 	}
-   }	
+   	
 	 		
 
