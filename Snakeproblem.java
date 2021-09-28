4808817d2a2dd0376d3d7e5c471821c9dcd9a568
captjack-4 <90202533+captjack-4@users.noreply.github.com>
2021-09-28 09:01:55 +0530

    Update Snakeproblem.java

--- Snakeproblem.java
+++ Snakeproblem.java
@@ -2,41 +2,41 @@ package snakeladder;
 
    public class Snakeproblem {
 	   public static void main(String[] args) {
-		    int snake=1, ladder=2, player=0, count=0;
-	        int player1=0, player2=0;
-	        while (player1<100 && player2<100) 
+		    int SNAKE=1, LADDER=2, Player=0, count=0;
+	        int Player1=0, Player2=0;
+	        while (Player1<100 && Player2<100) 
 	        {
 	            int dice=(int)(Math.random()*6+1);
-	            int check=(int)(Math.random()*3+1);
+	            int play=(int)(Math.random()*3+1);
 
-	            if (check==ladder) {
-	                player= player+dice;
-	                if (player>100)
-	                    player=player-dice;
+	            if (play==LADDER) {
+	                Player= Player+dice;
+	                if (Payer>100)
+	                    Player=Player-dice;
 	            }
-	            else if (check==snake) {
-	                player=player-dice;
-	                if (player<0)
-	                    player=0;
-	                else if (player>100)
-	                    player=player-dice;
+	            else if (play==SNAKE) {
+	                Player=Player-dice;
+	                if (Player<0)
+	                    Player=0;
+	                else if (Player>100)
+	                    Player=Player-dice;
 	            }
 	            if (count%2==0) {
-	                player1 = player;
-	                System.out.println("Player1 current position is: " +player1);
+	                Player1 = Player;
+	                System.out.println("Player1 current position is: " +Player1);
 	            }
 	            else {
-	                player2 = player;
-	                System.out.println("Player2 current position is: "+player2);
+	                Player2 = Player;
+	                System.out.println("Player2 current position is: "+Player2);
 	            }
 	            count++;
-//	          System.out.println("Player current position is: "+player);
+//	          System.out.println("Player current position is: "+Player);
 	        }
 	        System.out.println("\n"+count+" times the dice was played.\n");
-	        System.out.println(player1+" position");
-	        System.out.println(player2+" position.");
+	        System.out.println(Player1+" position");
+	        System.out.println(Player2+" position.");
 
-	        if (player1>player2)
+	        if (Player1>Player2)
 	            System.out.println("The winner of this game is Player1");
 	        else
 	            System.out.println("The winner of this game is Player2");
