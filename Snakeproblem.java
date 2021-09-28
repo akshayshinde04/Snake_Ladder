ecb47d81c480afd020257e542aa8a91f43a111de
captjack-4 <90202533+captjack-4@users.noreply.github.com>
2021-09-28 08:43:41 +0530

    Update Snakeproblem.java

--- Snakeproblem.java
+++ Snakeproblem.java
@@ -1,9 +1,9 @@
 package snakeladder;
 
-   public class Snakeproblem {
+   public class SnakeProblem {
 	   public static void main(String[] args) {
-		   int startposition = 0; 
-	    	 System.out.println("Start position "+startposition);		
+		int startposition = 0; 
+	    	System.out.println("Start position "+startposition);		
 	}
    }	
 	 		
