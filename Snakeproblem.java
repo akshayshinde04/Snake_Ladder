38235fb2cb1f80e08ba0a1a19b885459deadef15
captjack-4 <90202533+captjack-4@users.noreply.github.com>
2021-09-28 08:44:04 +0530

    Update Snakeproblem.java

--- Snakeproblem.java
+++ Snakeproblem.java
@@ -2,7 +2,7 @@ package snakeladder;
 
    public class Snakeproblem {
 	   public static void main(String[] args) {
-		   int snake=0 , ladder=0;
+	       int snake=0 , ladder=0;
 	       int startposition = 0; 
 	       System.out.println("Start position "+startposition);	
 	       int dies = (int)((Math.random()*10) % 6)+1;
