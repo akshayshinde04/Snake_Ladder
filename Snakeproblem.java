edda6e7fd015c587f1a3193cebe886d0d8330d31
unknown <akshayHVshinde04@gmail.com>
2021-09-28 07:11:32 +0530

    Added: UC4_PlayerReaches100Position

--- Snakeproblem.java
+++ Snakeproblem.java
@@ -2,26 +2,27 @@ package snakeladder;
 
    public class Snakeproblem {
 	   public static void main(String[] args) {
-	        int snake=1, ladder=2, player=15;
-	        int dice=(int)(Math.random()*6+1);
-	        System.out.println(dice);
+	        int snake=1, ladder=2, player=0;
+	        while (player<100) {
+	            int dice=(int)(Math.random()*6+1);
+	            System.out.println("The die number" +dice);
 
-	        int check=(int)(Math.random()*3+1);
-	        if (check==snake) {
-	            player= player-dice;
-	            System.out.println("Snake bite");
-	        }
-	        else if (check==ladder) {
-	            player=player+dice;
-	            System.out.println("U get Ladder");
-	        }
-	        else {
-	            player=player+dice;
-	            System.out.println("NO Play");
-	        }
-	        System.out.println("Player position is: "+player);
+	            int check=(int)(Math.random()*3+1);
+	            if (check==ladder) {
+	                player= player+dice;
+	            }
+	            else if (check==snake) {
+	                player=player-dice;
+	                if (player<0) {
+	                    player=0;
+	                }
+	            }
+	            else {
+	                player=player+dice;
+	            }
+	            System.out.println("Player current position is: "+player);
 	    }
-	}
-   	
+	   }
+   }
 	 		
 
