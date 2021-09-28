8c56aa1563ee8da6cfcb134b572d3c75a925db3d
captjack-4 <90202533+captjack-4@users.noreply.github.com>
2021-09-28 07:17:00 +0530

    Update Snakeproblem.java

--- Snakeproblem.java
+++ Snakeproblem.java
@@ -2,8 +2,8 @@ package snakeladder;
 
    public class Snakeproblem {
 	   public static void main(String[] args) {
-	        int snake=1, ladder=2, player=0;
-	        while (player<100) {
+	            int snake=1, ladder=2, player=0;
+	            while (player<100) {
 	            int dice=(int)(Math.random()*6+1);
 	            System.out.println("The die number" +dice);
 
