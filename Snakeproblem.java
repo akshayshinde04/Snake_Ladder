02b979adf55ee06630a8716985dd719bb792d5bc
unknown <akshayHVshinde04@gmail.com>
2021-09-28 07:19:42 +0530

    Added: UC5_ExactWinning100

--- Snakeproblem.java
+++ Snakeproblem.java
@@ -2,27 +2,31 @@ package snakeladder;
 
    public class Snakeproblem {
 	   public static void main(String[] args) {
-	        int snake=1, ladder=2, player=0;
-	        while (player<100) {
+	            int snake=1, ladder=2, player=0;
+	            while (player<100) {
 	            int dice=(int)(Math.random()*6+1);
-	            System.out.println("The die number" +dice);
+	            System.out.println("The die number "+dice);
 
 	            int check=(int)(Math.random()*3+1);
 	            if (check==ladder) {
 	                player= player+dice;
+	                if (player>100)
+	                    player=player-dice;
 	            }
 	            else if (check==snake) {
 	                player=player-dice;
-	                if (player<0) {
+	                if (player<0)
 	                    player=0;
-	                }
+	                else if (player>100)
+	                    player=player-dice;
 	            }
 	            else {
-	                player=player+dice;
+	                player=player+0;
 	            }
 	            System.out.println("Player current position is: "+player);
-	    }
+	        }
 	   }
-   }
+   } 
+   
 	 		
 
